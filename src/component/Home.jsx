f2d858de866bec87a7f2a7422aa87b4280e04623
Bhanu pratap <142300187+Bhanupratap7248@users.noreply.github.com>
2025-06-25 09:56:40 +0530

    navbar

---
+++ src/component/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <>
+        <nav className='h-16 w-full bg-blue-300'> <h2 className='text-black'>Fitness Tracker</h2> </nav>
+    </>
+  )
+}
+
+export default Home
